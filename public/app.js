a7a252342435543791e51e2da88983b22294f855
Barbara Liau <bj.liau@gmail.com>
2016-11-25 16:36:27 -0700

    Add generate keypair click event

--- public/app.js
+++ public/app.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
   // All jQuery/JavaScript code will go in here
+  console.log('jquery ready')
 
-})
+  // Generate keypair
+  $('.keypair-btn--generate').on('click', function(event) {
+    event.preventDefault();
+    console.log('Generate Key Pair button clicked');
+    $.ajax({
+      method: 'GET',
+      url: '/keypair/generate'
+    });
+  });
+
+});
