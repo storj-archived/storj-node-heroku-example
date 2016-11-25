420fcf4ef59d895405d64c59df2af2d90804fae1
Barbara Liau <bj.liau@gmail.com>
2016-11-25 12:59:11 -0700

    Add comment

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const app = express();
 const fs = require('fs');
 const async = require('async');
-const app = express();
 const path = require('path');
 const localAssetsDir = __dirname + '/public';
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+
+/* Endpoints */
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
